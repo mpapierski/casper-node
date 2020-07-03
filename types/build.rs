a3023a2630f5109749f72ce44fbebc9633484d51
Michał Papierski <michal@papierski.net>
2020-07-03 17:58:23 +0200

    NDRS-90: Fmt.

--- types/build.rs
+++ types/build.rs
@@ -3,7 +3,11 @@ use rustc_version::{version_meta, Channel, VersionMeta};
 
 fn main() {
     #[cfg(not(feature = "no-unstable-features"))]
-    if let Ok(VersionMeta { channel: Channel::Stable, .. }) = version_meta() {
+    if let Ok(VersionMeta {
+        channel: Channel::Stable,
+        ..
+    }) = version_meta()
+    {
         println!("cargo:rustc-cfg=feature=\"no-unstable-features\"");
-   }
+    }
 }
